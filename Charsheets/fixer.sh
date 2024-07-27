579a3f1c7e756a5d2ba3e3c959369750ef8e6dd8
Louis Wasserman <wasserman.louis@gmail.com>
2024-07-27 14:29:50 -0700

    More automatic find/replace fixes.

--- Charsheets/fixer.sh
+++ Charsheets/fixer.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-# Note not all have/has can be autocorrected: "doesn't he have a carrot" is correct, "he have a carrot" isn't.
+ls | xargs sed -i 's/\\c\([A-Za-z]*){\([A-Za-z]*\)}/\\c\1{\\\2}/g'                        # Identify and correct missing backslashes: \cCharacter{Foo} -> \cCharacter{\Foo}
 
-ls | xargs sed -i 's/\\\([Tt]\)hey\ \\\(are|were|do|have\)/\\\1hey\2/g'                      # \They \are -> \Theyare, etc.  Correct for "have" because it's already been backslashed.
-ls | xargs sed -i "s/\\\([Tt]\)hey\ \\\(are|were|do|have\)nt/\\\1hey\2nt/g"                  # \They \arent -> \Theyarent
-ls | xargs sed -i 's/\\c\([A-Za-z]*){\([A-Za-z]*\)}/\\c\1{\\\2}/g'                           # Identify and correct missing backslashes: \cCharacter{Foo} -> \cCharacter{\Foo}
+ls | xargs sed -i 's/\\\([Tt]\)hey\ \\\(are|were|have\)/\\\1hey\2/g'                      # \cCharacter{\they \are} -> \Theyare, etc
+ls | xargs sed -i 's/\\c\([A-Za-z]*\){\\\([tT]\)hey} \\c\1{\\\(are|were|have\)}/\\c\1{\\\2\3}/g' # \cCharacter{\They} \cCharacter{\are} -> \cCharacter{\Theyare}, for both capitalizations, are, were, have
 
-ls | xargs sed -i 's/\\c\([A-Za-z]*){\\\([Tt]\)hey} \(is|are\)/\\c\1{\\\2heyare\3}/g'        # \cCharacter{\They} is/are -> \cCharacter{\Theyare}
-ls | xargs sed -i "s/\\c\([A-Za-z]*){\\\([Tt]\)hey} \(is|are\)n't/\\c\1{\\\2heyarent\3}/g"   # \cCharacter{\They} isn't/aren't -> \cCharacter{\Theyare}
+ls | xargs sed -i 's/\\c\([A-Za-z]*){\\\([Tt]\)hey} \(is|are\)/\\c\1{\\\2heyare\3}/g'     # \cCharacter{\They} is/are -> \cCharacter{\Theyare}
+ls | xargs sed -i "s/\\c\([A-Za-z]*){\\\([Tt]\)hey} \(was|were\)/\\c\1{\\\2heywere\3}/g"  # \cCharacter{\They} was/were -> \cCharacter{\Theywere}
+# have/has can't be corrected automatically: "doesn't he have a carrot" is correct, "he have a carrot" isn't.
 
-ls | xargs sed -i "s/\\c\([A-Za-z]*){\\\([Tt]\)hey} \(was|were\)/\\c\1{\\\2heywere\3}/g"            # \cCharacter{\They} was/were -> \cCharacter{\Theywere}
-ls | xargs sed -i "s/\\c\([A-Za-z]*){\\\([Tt]\)hey} \(was|were\)n't/\\c\1{\\\2heywerent\3}/g"       # \cCharacter{\They} wasn't/weren't -> \cCharacter{\Theywerent}
+ls | xargs sed -i "s/\\c\([A-Za-z]*){\\\([Tt]\)hey}'\(re|s\)/\\c\1{\\\2heyre}/g"     # \cCharacter{\They}'re -> \cCharacter{\Theyre}
+ls | xargs sed -i "s/\\c\([A-Za-z]*){\\\([Tt]\)hey}'ve/\\c\1{\\\2heyve}/g"     # \cCharacter{\They}'ve -> \cCharacter{\Theyve}
 
-ls | xargs sed -i "s/\\c\([A-Za-z]*){\\\([Tt]\)hey} \(do|does\)/\\c\1{\\\2heydo\3}/g"       # \cCharacter{\They} do/does -> \cCharacter{\They} do\cCharacter{erbes}
+ls | xargs sed -i 's/\(is|are\) \\c\([A-Za-z]*){\\they}/\\c\2{\\arethey}/g'               # is/are \cCharacter{\they} -> \cCharacter{\arethey}
+ls | xargs sed -i 's/\(was|were\) \\c\([A-Za-z]*){\\they}/\\c\2{\\werethey}/g'            # was/were \cCharacter{\they} -> \cCharacter{\werethey}
+ls | xargs sed -i 's/\(has|have\) \\c\([A-Za-z]*){\\they}/\\c\2{\\havethey}/g'            # has/have \cCharacter{\they} -> \cCharacter{\havethey}
 
+ls | xargs sed -i 's/\(Is|Are\) \\c\([A-Za-z]*){\\they}/\\c\2{\\Arethey}/g'               # Is/Are \cCharacter{\they} -> \cCharacter{\Arethey}
+ls | xargs sed -i 's/\(Was|Were\) \\c\([A-Za-z]*){\\they}/\\c\2{\\Werethey}/g'            # Was/Were \cCharacter{\they} -> \cCharacter{\Werethey}
+ls | xargs sed -i 's/\(Has|Have\) \\c\([A-Za-z]*){\\they}/\\c\2{\\Havethey}/g'            # Has/Have \cCharacter{\they} -> \cCharacter{\Havethey}
 
-ls | xargs sed -i 's/\\c\([A-Za-z]*){\\\([Tt]\)hey} does/\\c\1{\\\2heydo\3}/g'    # \cCharacter{\They} does -> \cCharacter{\They} do\cCharacter{erbes}
-ls | xargs sed -i 's/\(Are|Were|Do\) \\c\([A-Za-z]*){\\they}/\\c\2{\\\1they}/g'    # Are \cCharacter{\they} -> \cCharacter{\Arethey}
-ls | xargs sed -i "s/Don't \\c\([A-Za-z]*){\\they}/\\c\1{\\Dontthey}/g"    # Don't \cCharacter{\they} -> \cCharacter{\Dontthey}
+ls | xargs sed -i "s/“\([^“”]\)”/``\1''/g"                                                # “curly quoted” -> ``curly quoted''
 
+ls | xargs sed -i 's/^\\c\([A-Za-z]*\){\\they/\\c\1{\\They/g'                             # uncapitalized \they at the beginning of a paragraph(also \theyare, etc)
+ls | xargs sed -i 's/\.\(\s+\)\\c\([A-Za-z]*\){\\they/.\1\\c\1{\\They/g'                  # uncapitalized \they at the beginning of a sentence within a paragraph
+
+### Eliminate \do.
+ls | xargs sed -i 's/\\c\([a-zA-Z]*\){\\do}/do\\c\1{\\verbes}/g'                          # \cCharacter{\do} -> do\cCharacter{\verbes}
+ls | xargs sed -i 's/\\c\([a-zA-Z]*\){\\\([tT]\)heydo}/\\c\1{\\\2hey} do\\c\1{\\verbes}/g' # \cCharacter{\theydo} -> \cCharacter{\they} do\cCharacter{\verbes}
