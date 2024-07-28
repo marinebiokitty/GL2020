08f36dbb9dae22419c3e8f063e3b9e52dd4a3a6e
Louis Wasserman <wasserman.louis@gmail.com>
2024-07-28 09:50:28 -0700

    Document, debug, and handle errors more smoothly in Introfier.

--- Charsheets/Introfier.java
+++ Charsheets/Introfier.java
@@ -3,26 +3,35 @@ import java.nio.file.*;
 import java.util.*;
 import java.util.regex.*;
 
+/**
+ * Simple script that accepts a list of file names, and in those files, replaces the first reference 
+ * to each character \cCharacter with \cCharacter{\intro}.
+ */
 public class Introfier {
     private static final Pattern CHARACTER_REFERENCE = Pattern.compile(
         "(?<!\\{)\\\\c([A-Z][A-Za-z]*)\\{(\\\\[A-Za-z]+)?\\}"
-    );
+    ); // \cStartsWithUpperCase{\optionallyOneCommand}, *not* preceded by a {
 
     public static void main(String[] args) throws IOException {
         for (String file : args) {
             Path path = Paths.get(file);
-            String contents = Files.readString(path);
-            Set<String> seenCharacters = new HashSet<>();
-            Matcher matcher = CHARACTER_REFERENCE.matcher(contents);
-            String result = matcher.replaceAll(matchResult -> {
-                String charCommand = matchResult.group(1);
-                if (!seenCharacters.add(charCommand)) {
-                    return "$0"; // we've already seen this character; don't replace anything
-                } else {
-                    return "\\\\c$1{\\\\intro}";
-                }
-            });
-            Files.writeString(path, result);
+            try {
+                String contents = Files.readString(path);
+                Set<String> seenCharacters = new HashSet<>();
+                Matcher matcher = CHARACTER_REFERENCE.matcher(contents);
+                String result = matcher.replaceAll(matchResult -> {
+                    String charCommand = matchResult.group(1);
+                    if (!seenCharacters.add(charCommand)) {
+                        return "$0"; // we've already seen this character; don't replace anything
+                    } else {
+                        return "\\\\c$1{\\\\intro}";
+                    }
+                });
+                Files.writeString(path, result);
+                System.out.println("Processed file " + file + ".");
+            } catch (Exception e) {
+                new IOException("Error on file " + file + ".  Continued.", e).printStackTrace();
+            }
         }
     }
 }
